1e90e459e5d161157e81b57cd8ecfb289879371b
Finn Else-McCormick <2200864@uad.ac.uk>
2024-03-03 21:30:56 +0000

    Added glfw, imgui and implot

--- astar-parallel/src/main.cpp
+++ astar-parallel/src/main.cpp
@@ -1,6 +1,12 @@
 #include "Graph/DirectedGraph.h"
 
 #include <iostream>
+#include <GLFW/glfw3.h>
+
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+
 
 template<class ValueType, class WeightType>
 void outputGraph(const DirectedGraph<ValueType, WeightType>& graph) {
@@ -21,5 +27,45 @@ int main() {
 
 	outputGraph(graph);
 
+	if (!glfwInit()) {
+		std::cout << "GLFW failed to initialise." << std::endl;
+		return 0;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable vsync
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		ImGui::ShowDemoWindow(); // Show demo window! :)
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwTerminate();
 	return 0;
 }
